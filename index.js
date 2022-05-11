e157d3506ffd1a73240cf59875fc7a1df7921ac4
Alberto Alonso <alberto.alonso@unity3d.com>
2022-05-11 07:46:12 +0300

    Send a random quote

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const { IncomingWebhook } = require('@slack/webhook');
 const core = require('@actions/core');
+const axios = require('axios').default;
 
-try {
-  const url = core.getInput('incoming-webhook');
-  const webhook = new IncomingWebhook(url);
-  webhook.send('Hello world');
-} catch (error) {
-  core.setFailed(error.message);
+const getRandomQuote = async (url) => {
+  const res = await axios.get(url);
+  const quotes = res.data.data;
+  const n = Math.floor(Math.random() * quotes.length);
+  return quotes[n].quote;
 }
+
+(async() => {
+  try {
+    const url = core.getInput('incoming-webhook');
+    const quotesUrl = core.getInput('quotes-url') || 'https://your-fortune.github.io/data/murphys-law.json';
+    
+    const webhook = new IncomingWebhook(url);
+    webhook.send(await getRandomQuote(quotesUrl));
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
